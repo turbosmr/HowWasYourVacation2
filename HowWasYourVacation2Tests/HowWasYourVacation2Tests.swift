7d3bb7fe2926293d879b2f6f75ee3aaf14495e15
Steve Rentschler <turbo_smr@msn.com>
2020-03-25 19:57:46 -0700

    Initial Commit

---
+++ HowWasYourVacation2Tests/HowWasYourVacation2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HowWasYourVacation2Tests.swift
+//  HowWasYourVacation2Tests
+//
+//  Created by STEVE RENTSCHLER on 3/25/20.
+//  Copyright © 2020 STEVE RENTSCHLER. All rights reserved.
+//
+
+import XCTest
+@testable import HowWasYourVacation2
+
+class HowWasYourVacation2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
